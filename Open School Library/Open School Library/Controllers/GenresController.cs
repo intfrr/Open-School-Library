a942d544e28a0f2eb1296dcf33ed98b95e4a0989
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2016-05-04 21:47:11 -0400

    Update Genres Create View to work with the new OSLModel.

--- Open School Library/Open School Library/Controllers/GenresController.cs
+++ Open School Library/Open School Library/Controllers/GenresController.cs
@@ -46,7 +46,7 @@ namespace Open_School_Library.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,genre")] Genre Genre)
+        public ActionResult Create([Bind(Include = "GenreID,GenreName")] Genre Genre)
         {
             if (ModelState.IsValid)
             {
